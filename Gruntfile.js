a6c856be0d22dac020b1f52bbfdc7619ff83a307
robert.edgar <robertedgar@netvigator.com>
2014-12-29 06:36:59 -0500

    first test

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,8 +39,6 @@ module.exports = function(grunt) {
       },
       custom_options: {
         options: {
-          separator: ': ',
-          punctuation: ' !!!'
         },
         files: {
           'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
--- tasks/cxsvg.js
+++ tasks/cxsvg.js
@@ -4,12 +4,12 @@
  *
  * Copyright (c) 2014 robert.edgar
  * Licensed under the MIT license.
+ * Optimize individual svg files and generate a single svg container file with embedded symbols
  */
 
 'use strict';
 
 //imports
-var FS = require('fs');
 var SVGO = require('svgo');
 
 module.exports = function(grunt) {
@@ -30,44 +30,61 @@ module.exports = function(grunt) {
       grunt.verbose.writeln.apply(null, arguments);
     }
   };
+  var rxEnableBg = /enable-background="[^"]*"/g;
+  var rxStripGrid = /<g[^>]*>(.*)?<\/g>/g;
+  var rxStripSize = / (height|width|x|y)="[^"]*"/g;
+
+  /**
+   * Helper function to map svg file names to view 'pod' containing a "use" tag
+   * @param {string} name svg filename including extension
+   * @returns {string}
+   */
+  function useMapHelper(name) {
+    return '<div class="pod"><svg><use xlink:href="#' + name.replace('\.svg','') + '" /></svg><div class="title">'+name.replace('\.svg','') +'</div></div>';
+  }
+
 
   /**
    * Process the next file
    * @param {Array} stack
    * @param {object}options
-   * @param {fs.WriteStream }[stream]
    */
-  function next(stack, options, stream){
-    if(!stream) {
+  function next(stack, options){
+    if(!options.useinfo) {
       //log start info
       logger.log('processing '+(stack.length-1)+' files');
-      //create stream and write header
-      stream = FS.createWriteStream(options.outputFile);
-
-      stream.write('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs>');
+      //store useinfo
+      options.useinfo = stack.map(useMapHelper).join('');
+      //write svg header
+      options.svginfo = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">';
     }
 
     var file = stack.shift();
     if(file){
       if(file===options.outputFile){
         //skip the output file
-        next(stack, options, stream);
+        next(stack, options);
       }else {
         try {
           svgo.optimize(grunt.file.read(options.dir+file), function (result) {
             logger.log('write '+file);
-            stream.write('<!--[' + (file.replace('.svg', '')) + ']-->'+result.data);
-            next(stack, options, stream);
+            var svg = result.data.replace('<svg ', '<symbol id="'+(file.replace('.svg', ''))+'"').replace('<\/svg>','<\/symbol>').replace('xmlns="http://www.w3.org/2000/svg"','').replace(rxEnableBg,'').replace(rxStripSize,'').replace(rxStripGrid,'');
+            options.svginfo+=svg;
+            next(stack, options);
           });
         } catch (e) {
           //log error and continue
           logger.error(e);
-          next(stack, options, stream);
+          next(stack, options);
         }
       }
     }else{
-      //write footer
-      stream.end('</defs></svg>');
+      //write svg tail
+      options.svginfo+='</svg>';
+      //write main output svg file
+      grunt.file.write(options.dir+options.outputFile, options.svginfo);
+      //write html view page
+      grunt.file.write(options.dir+'icons.html', '<!doctype html><html><head><style>.pod{float:left;width:100px;height:100px;text-align:center;border: 1px solid #666;margin: 5px;padding: 5px;}svg{width:50px;height:50px;fill:black !important;}</style><head><body>'+options.svginfo+options.useinfo+'</body></html>');
       //complete
       logger.log('all done');
     }
@@ -76,7 +93,7 @@ module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerTask('cxsvg', 'The best Grunt plugin ever.', function() {
+  grunt.registerTask('cxsvg', 'The cx svg optimizer plugin.', function() {
     //load options
     var options = this.options({dir: 'assets/svg/', outputFile: 'wjicons.svg'});
     //get file list from glob and start processing
